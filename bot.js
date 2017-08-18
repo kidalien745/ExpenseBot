a022727760d5f693bc0a6f746c17698a39e9f67b
Austin Lien <kidalien745@gmail.com>
2017-08-18 13:42:23 -0500

    Only respond when tagged

--- bot.js
+++ bot.js
@@ -4,9 +4,10 @@ var botID = process.env.BOT_ID;
 
 function respond()
 {
-    var request = JSON.parse(this.req.chunks[0]);
+    var request = JSON.parse(this.req.chunks[0]),
+        botRegex = /@ExpenseBot/;
 
-    if (request.text && request.text != "This is a message!") {
+    if (request.text && botRegex.test(request.text)) {
         this.res.writeHead(200);
         postMessage();
         this.res.end();
